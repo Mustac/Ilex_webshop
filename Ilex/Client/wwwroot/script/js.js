2d19d79190118d87651ac80eb865b4e47ab9725a
Mustac <mustac.marijan@gmail.com>
2021-06-20 02:54:49 +0200

    Pocetak, Trgovina, ONama design implemented.
    Google Maps working

---
+++ Ilex/Client/wwwroot/script/js.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(() => {
+    $('.nav-button').click(() => {
+        $('.nav-button').toggleClass('change');
+    });
+
+    navigation();
+   
+
+    $(window).scroll(() => {
+        navigation();
+    });
+
+    var pathname = window.location.pathname;
+
+
+    /* navigation */
+
+    function navigation() {
+        let position = $(this).scrollTop();
+        console.log(position);
+
+        if (position >= 61) {
+            $(".navbar").removeClass("top-nav");
+        } else {
+            if (!$(".navbar").hasClass("top-nav")) {
+                $(".navbar").addClass("top-nav");
+            }
+        }
+    };
+
+    function buttonNavBarClick() {
+        $('.nav-button').click(() => {
+
+            $('.nav-button').toggleClass('change');
+
+
+        });
+    };
+
+   
+
+});
+
+
+window.initializeCarousel = () => {
+    $('#carousel').carousel({ interval: 7000 });
+}
+
